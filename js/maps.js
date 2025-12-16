487030f1c7d53897bc3de219f720fdeb153aaca2
byte <sadness110as@gmail.com>
2025-12-16 12:13:12 -0500

    upFinal2

--- js/maps.js
+++ js/maps.js
@@ -1,9 +1,9 @@
 const map = L.map("map").setView([-15.8402, -70.0219], 13);
-console.log(map);
+
 let y=L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors"
 }).addTo(map);
-console.log(y);
+
 L.marker([-15.8402, -70.0219])
   .addTo(map)
   .bindPopup("<b>Puno</b><br>Lago Titicaca")
