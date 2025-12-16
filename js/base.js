eea9f88acb85323058e7ed753c125ca09bfcb0cc
byte <sadness110as@gmail.com>
2025-12-16 12:12:07 -0500

    upFinal

--- js/base.js
+++ js/base.js
@@ -1,5 +1,53 @@
+const maps = L.map("map").setView([-15.8402, -70.0219], 13);
+
+let y=L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors"
+}).addTo(maps);
+
+L.marker([-15.8402, -70.0219])
+  .addTo(maps)
+  .bindPopup("<b>Puno</b><br>Lago Titicaca")
+  .openPopup();
+
+
+
 let btnMenu=document.querySelector('.btnMenu');
 let menu=document.querySelector('.menu');
 btnMenu.addEventListener('click',function(){
 	menu.classList.toggle('on');
 });
+
+
+let mapContent=document.querySelector('.map-content');
+let btnCloseMap=document.querySelector('.map-content .btn-close');
+let mapTitle=document.querySelector('.map-content .map-title');
+
+btnCloseMap.addEventListener('click',function(){
+	mapContent.classList.add('none');
+});
+
+let atractivos=document.querySelectorAll('.atractivos .card .card-link');
+for(let i=0;i<atractivos.length;i++){
+	atractivos[i].addEventListener('click',function(){
+		mapContent.classList.remove('none');
+		/*console.log(destinos[i]);*/
+		mapTitle.innerHTML=destinos[i].nombre;
+		L.marker([destinos[i].coords[0],destinos[i].coords[1]])
+  		.addTo(maps)
+  		.bindPopup(destinos[i].nombre)
+  		.openPopup();
+	});
+}
+
+let hoteles=document.querySelectorAll('.hoteles .card .card-link');
+for(let i=0;i<hoteles.length;i++){
+	hoteles[i].addEventListener('click',function(){
+		mapContent.classList.remove('none');
+		mapTitle.innerHTML=hotelesJson[i].nombre;
+		L.marker([hotelesJson[i].coords[0],hotelesJson[i].coords[1]])
+  		.addTo(maps)
+  		.bindPopup(hotelesJson[i].nombre)
+  		.openPopup();
+	});
+}
+
---
+++ js/rutas.js
@@ -0,0 +1,32 @@
+const destinos = [
+  {
+    nombre: "Ruinas de Sillustani",
+    coords: [-15.721266851540664, -70.15893328463488]
+  },
+  {
+    nombre: "Lago Titicaca",
+    coords: [-15.825,-69.325]
+  },
+  {
+    nombre: "Islas Flotantes de los Uros",
+    coords: [-15.8149,-69.9769]
+  },
+  {
+    nombre: "Chullpas de Cutimbo",
+    coords: [-15.833,-70.200]
+  }
+];
+const hotelesJson = [
+  {
+    nombre: "Kaaro Hotel Puno",
+    coords: [-15.839836026551595, -70.02992602392482]
+  },
+  {
+    nombre: "Lake Titicaca Hotel",
+    coords: [-15.822758115692585, -69.99515771349009]
+  },
+  {
+    nombre: "Hotel Hacienda Puno",
+    coords: [-15.839895882989305, -70.02688300411258]
+  }
+];
